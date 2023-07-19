6f8efc3dc85b24abed80a67e09598c6548548036
David Heredia <dherediat97@gmail.com>
2023-07-19 12:17:11 +0200

    Dependabot a las 11:11, mejorar la creacion de la release en Github, limpieza de codigo

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-import 'package:dio/dio.dart';
 import 'package:FilmFlu/env/env.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:FilmFlu/dto/movie.dart';
 
 const String baseURL = 'https://api.themoviedb.org/3';
@@ -15,27 +13,6 @@ Map<String, dynamic> baseHeaders = {
 };
 
 class Api {
-  final _dio = Dio();
-
-  Api() {
-    _dio.options = BaseOptions(
-      baseUrl: baseURL,
-      headers: baseHeaders,
-      connectTimeout: const Duration(milliseconds: 5000),
-      receiveTimeout: const Duration(milliseconds: 3000),
-    );
-    initializeInterceptors();
-  }
-
-  initializeInterceptors() {
-    _dio.interceptors.add(PrettyDioLogger(
-      requestBody: false,
-      responseBody: true,
-      responseHeader: false,
-      compact: true,
-    ));
-  }
-
   Movie parseMovie(String responseBody) {
     final parsed = jsonDecode(responseBody);
     debugPrint("parse=$parsed");
