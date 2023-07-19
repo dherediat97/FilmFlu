681fa74935832016c19b5e4474e8088095180e55
David Heredia <dherediat97@gmail.com>
2023-07-19 11:57:39 +0200

    Meter internalizacion en la app, meter los iconos en la app, y mejorar inicio de la app haciendo la peticion al empezar la pagina no despues

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+//Core Packages
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+//My Packages
 import 'package:FilmFlu/ui/screens/login/login_page.dart';
 import 'package:FilmFlu/ui/screens/splash/splash_screen.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(const FilmFlu());
@@ -12,24 +17,37 @@ class FilmFlu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Film Flu',
         debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        onGenerateTitle: (context) => AppLocalizations.of(context)!.app_name,
+        supportedLocales: [Locale('en'), Locale('es')],
         theme: ThemeData(
           fontFamily: 'Barlow',
           primaryColor: Color(0xFFC7CD18),
           useMaterial3: true,
           textTheme: const TextTheme(
-            bodyMedium:
-                TextStyle(color: Color(0xFFFFFFFF), fontFamily: 'Barlow'),
+            bodyMedium: TextStyle(
+              color: Color(0xFFFFFFFF),
+              fontFamily: 'Barlow',
+            ),
           ),
           colorScheme: ColorScheme.fromSwatch(
-              primarySwatch: Colors.yellow, backgroundColor: Color(0x10F0F)),
+            primarySwatch: Colors.yellow,
+            backgroundColor: Color(0x10F0F),
+          ),
         ),
         darkTheme: ThemeData(
           primaryColor: Color(0xFFC7CD18),
           useMaterial3: true,
           colorScheme: ColorScheme.fromSwatch(
-              primarySwatch: Colors.yellow, backgroundColor: Color(0x10F0F)),
+            primarySwatch: Colors.yellow,
+            backgroundColor: Color(0x10F0F),
+          ),
         ),
         initialRoute: '',
         routes: {
