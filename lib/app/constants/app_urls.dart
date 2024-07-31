6c5cff015ac7fcf15525dcbc0081d570bb10b937
dherediat <dherediat97@gmail.com>
2024-07-31 21:39:45 +0200

    feature: migrate and update image loaders

--- lib/app/constants/app_urls.dart
+++ lib/app/constants/app_urls.dart
@@ -1,13 +1,10 @@
 class AppUrls {
   static const String baseUrl = 'https://api.themoviedb.org/3/';
-  static const String movieImgBaseURL =
-      'https://image.tmdb.org/t/p/w300_and_h450_bestv2';
+  static const String movieImgBaseURL = 'https://image.tmdb.org/t/p/w200';
   static const String movieLandscapeBaseUrl =
       'https://image.tmdb.org/t/p/original';
-  static const String personImgBaseUrl =
-      'https://image.tmdb.org/t/p/w600_and_h900_bestv2';
-  static const String profileImgBaseUrl =
-      'https://secure.gravatar.com/avatar/';
+  static const String personImgBaseUrl = 'https://image.tmdb.org/t/p/w200';
+  static const String profileImgBaseUrl = 'https://secure.gravatar.com/avatar/';
 
   AppUrls._();
 }
--- lib/presentation/widgets/default_image_loader.dart
+++ lib/presentation/widgets/default_image_loader.dart
@@ -1,31 +1,37 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:film_flu/presentation/widgets/placeholder_loader.dart';
 import 'package:flutter/material.dart';
-import 'package:octo_image/octo_image.dart';
 
-class DefaultImageLoader extends StatefulWidget {
+class DefaultImageLoader extends StatelessWidget {
   const DefaultImageLoader({
     super.key,
     required this.imageUrl,
+    this.darkenImage = false,
   });
 
   final String? imageUrl;
+  final bool? darkenImage;
 
-  @override
-  State<DefaultImageLoader> createState() => _DefaultImageLoaderState();
-}
-
-class _DefaultImageLoaderState extends State<DefaultImageLoader> {
   @override
   Widget build(BuildContext context) {
-    var imageWidget = OctoImage(
-      image: CachedNetworkImageProvider(
-        widget.imageUrl!,
-      ),
-      fit: BoxFit.cover,
-      progressIndicatorBuilder:
-          OctoProgressIndicator.circularProgressIndicator(),
-    );
-    return widget.imageUrl != null ? imageWidget : const PlaceholderLoader();
+    return imageUrl != null
+        ? CachedNetworkImage(
+            imageUrl: imageUrl!,
+            fit: BoxFit.cover,
+            filterQuality: FilterQuality.low,
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            progressIndicatorBuilder: (context, url, progress) =>
+                const Center(child: PlaceholderLoader()),
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                    colorFilter: const ColorFilter.mode(
+                        Colors.white60, BlendMode.darken)),
+              ),
+            ),
+          )
+        : const PlaceholderLoader();
   }
 }
